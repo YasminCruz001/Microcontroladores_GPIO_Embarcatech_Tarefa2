c73e9bcc7f4a9b964896dd20f523b00ef9f0c782
YasminCruz001 <damascenocruzyasmin@gmail.com>
2025-01-19 22:20:31 -0300

    Função Configurar gpio e Main

--- Microcontroladores_GPIO_Tarefa2.c
+++ Microcontroladores_GPIO_Tarefa2.c
@@ -39,3 +39,93 @@ void emitir_som(int duracao_ms, int frequencia_hz)
         sleep_us(atraso_microsegundos);  // Aguarda novamente
     }
 }
+
+// Função para configurar um pino GPIO como saída
+void configurar_gpio(int pino)
+{
+    gpio_init(pino);              
+    gpio_set_dir(pino, GPIO_OUT); 
+    gpio_put(pino, false);        
+}
+
+int main()
+{
+    stdio_init_all();  
+
+    // Configura os pinos dos LEDs e do buzzer
+    configurar_gpio(LED_VERDE_PIN);
+    configurar_gpio(LED_AZUL_PIN);
+    configurar_gpio(LED_VERMELHO_PIN);
+    configurar_gpio(BUZZER_PIN);
+
+    char comando[10];  // Vetor para armazenar o comando digitado pelo usuário
+
+    while (true)
+    {
+        scanf("%9s", comando);  
+        printf("%s\n", comando); 
+
+        
+        if (strcmp(comando, "verde") == 0)
+        {
+            gpio_put(LED_AZUL_PIN, false);
+            gpio_put(LED_VERDE_PIN, true);
+            gpio_put(LED_VERMELHO_PIN, false);
+        }
+        else if (strcmp(comando, "azul") == 0)
+        {
+            gpio_put(LED_AZUL_PIN, true);
+            gpio_put(LED_VERDE_PIN, false);
+            gpio_put(LED_VERMELHO_PIN, false);
+        }
+        else if (strcmp(comando, "vermelho") == 0)
+        {
+            gpio_put(LED_AZUL_PIN, false);
+            gpio_put(LED_VERDE_PIN, false);
+            gpio_put(LED_VERMELHO_PIN, true);
+        }
+        else if (strcmp(comando, "branco") == 0)
+        {
+            gpio_put(LED_AZUL_PIN, true);
+            gpio_put(LED_VERMELHO_PIN, true);
+            gpio_put(LED_VERDE_PIN, true);
+        }
+        else if (strcmp(comando, "ciano") == 0)
+        {
+            gpio_put(LED_VERMELHO_PIN, false);
+            gpio_put(LED_VERDE_PIN, true);
+            gpio_put(LED_AZUL_PIN, true);
+        }
+        else if (strcmp(comando, "roxo") == 0)
+        {
+            gpio_put(LED_VERMELHO_PIN, true);
+            gpio_put(LED_VERDE_PIN, false);
+            gpio_put(LED_AZUL_PIN, true);
+        }
+        else if (strcmp(comando, "amarelo") == 0)
+        {
+            gpio_put(LED_VERMELHO_PIN, true);
+            gpio_put(LED_VERDE_PIN, true);
+            gpio_put(LED_AZUL_PIN, false);
+        }
+        else if (strcmp(comando, "buzzer") == 0)
+        {
+            emitir_som(3000, 300);  // Emite um som no buzzer por 3 segundos com 300 Hz
+        }
+        else if (strcmp(comando, "off") == 0)
+        {
+            
+            gpio_put(LED_VERMELHO_PIN, false);
+            gpio_put(LED_VERDE_PIN, false);
+            gpio_put(LED_AZUL_PIN, false);
+        }
+        else if (strcmp(comando, "reboot") == 0)
+        {
+            reset_usb_boot(0, 0);  // Reinicia o dispositivo
+        }
+        else
+        {
+            printf("Comando inválido, tente novamente!\n");  
+        }
+    }
+}
